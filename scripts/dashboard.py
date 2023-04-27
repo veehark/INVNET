4a8938f7ed8e70d3f08e2baa5fe9ffa4db03de8b
lhhonk <lhj.honkanen@gmail.com>
2023-04-27 19:20:01 +0300

    asdadadsadadasa

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -17,46 +17,60 @@ app=Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 ##########################################
 ticker="^OMXHGI"
 
-OMXHGI=yf.download(ticker,
+df=yf.download(ticker,
                      start=None,
                      end=None,
                      progress=False)
 
-OMXHGI=OMXHGI.loc[:,["Adj Close"]]
+df=df.loc[:,["Adj Close"]]
 
 ##########################################
 #Return calculations
 ##########################################
-OMXHGI["simple_rtn"]=OMXHGI["Adj Close"].pct_change()
-OMXHGI["log_rtn"]=np.log(OMXHGI["Adj Close"]/OMXHGI["Adj Close"].shift(1))
-OMXHGI["cumulative_simple_rtn"]=(OMXHGI["Adj Close"]/OMXHGI.iloc[0]['Adj Close']-1)
-OMXHGI=OMXHGI.dropna()
-
+df["simple_rtn"]=df["Adj Close"].pct_change()
+df["log_rtn"]=np.log(df["Adj Close"]/df["Adj Close"].shift(1))
+df["cumulative_simple_rtn"]=(df["Adj Close"]/df.iloc[0]['Adj Close']-1)
+df=df.dropna()
+print(df.head())
 ##########################################
 #Building graphs and tables
 ##########################################
-table_desc=pd.DataFrame(OMXHGI.loc[:,'simple_rtn'].describe(include='all'))
+table_desc=pd.DataFrame(df.loc[:,'simple_rtn'].describe(include='all'))
 
-fig_price=px.line(OMXHGI, y='Adj Close')
+fig_price=px.line(df, y='Adj Close')
 fig_price.update_layout(margin=dict(l=2, r=2, b=5, t=5),
                             xaxis=dict(showspikes=True,
                                         spikethickness=1,
                                         spikedash='dot',
                                         spikemode='across'))
 
-fig_rtn=px.line(OMXHGI, y='simple_rtn').update_traces(line_color='gray')
+fig_rtn=px.line(df, y='simple_rtn').update_traces(line_color='gray')
 fig_rtn.update_layout(margin=dict(l=2, r=2, b=5, t=5),
                       xaxis=dict(showspikes=True,
                                     spikethickness=1,
                                     spikedash='dot',
                                     spikemode='across'))
 
-fig_hist=px.histogram(OMXHGI, x='simple_rtn', nbins=1000)
+fig_hist=px.histogram(df, x='simple_rtn', nbins=1000)
 fig_hist.update_layout(margin=dict(l=2, r=2, b=5, t=5),
                        xaxis=dict(showspikes=True,
                                     spikethickness=1,
                                     spikedash='dot',
                                     spikemode='across'))
+
+#Underwater plot
+# Calculate drawdowns
+cum_max = df['Adj Close'].cummax()
+drawdown = (df['Adj Close'] - cum_max) / cum_max
+
+# Create Plotly Express figure
+fig_drawdown = px.area(df, color=drawdown < 0, 
+              color_discrete_sequence=['blue', 'red'], 
+              title='Underwater Plot of Asset Returns',
+              labels={'index': 'Date', 'Adj Close': 'Adj Close'})
+fig_drawdown.update_layout(yaxis_tickformat=',.2f',
+                  xaxis_rangeslider_visible=True,
+                  margin=dict(l=2, r=2, b=5, t=35))
 
 ##########################################
 #Dash building
@@ -72,13 +86,17 @@ app.layout=dbc.Container([
     ], justify="between"),
     dbc.Row([
         dbc.Col([
-            dash_table.DataTable(table_desc.to_dict('records'),
-                                 [{"name": i, "id": i} for i in table_desc.columns])
-        ], width=3),
+            dcc.Graph(figure=fig_drawdown, config={'displayModeBar':False, 'scrollZoom':False, 'dragMode':False, 'editable':False})
+        ], width=6),
         dbc.Col([
             dcc.Graph(figure=fig_hist, config={'displayModeBar':False, 'scrollZoom':False, 'dragMode':False, 'editable':False})
         ], width=6)
     ], justify="between"),
+    dbc.Row([
+        dbc.Col([
+                dash_table.DataTable(table_desc.to_dict('records'))
+            ], width=3),
+    ])
 ])
 
 if __name__ == '__main__':
